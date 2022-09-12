206efa7e174b043359d046962de4e343bf6107c6
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-12 18:41:33 -0300

    Configuring hr-config-server project

--- hr-config-server/src/main/java/com/yamamoto64/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/yamamoto64/hrconfigserver/HrConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.yamamoto64.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
 

27ff851997393e817e9c656aee99a2230ed337a1
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-12 12:08:38 -0300

    project hr-payroll created

---
+++ hr-payroll/src/test/java/com/yamamoto64/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamamoto64.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

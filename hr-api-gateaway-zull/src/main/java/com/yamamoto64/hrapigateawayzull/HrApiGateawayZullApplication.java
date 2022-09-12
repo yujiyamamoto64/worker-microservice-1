c6f0b151688d7dbf39ae03b94fd47ee49acadd3d
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-12 16:25:45 -0300

    Project hr-api-gateaway-zull created

---
+++ hr-api-gateaway-zull/src/main/java/com/yamamoto64/hrapigateawayzull/HrApiGateawayZullApplication.java
@@ -0,0 +1,13 @@
+package com.yamamoto64.hrapigateawayzull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGateawayZullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGateawayZullApplication.class, args);
+	}
+
+}

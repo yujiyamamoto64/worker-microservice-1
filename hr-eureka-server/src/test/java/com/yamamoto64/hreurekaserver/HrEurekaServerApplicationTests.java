914e14cf2c3b6c19e4f2cd7823bc031ccc4cbe4f
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-12 14:54:50 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/yamamoto64/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamamoto64.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

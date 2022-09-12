347817bc3fcbddde56773da3203ecaed3a02f4cb
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-12 11:03:41 -0300

    Project hr-work created

---
+++ hr-worker/src/main/java/com/yamamoto64/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.yamamoto64.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
---
+++ hr-worker/src/test/java/com/yamamoto64/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamamoto64.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

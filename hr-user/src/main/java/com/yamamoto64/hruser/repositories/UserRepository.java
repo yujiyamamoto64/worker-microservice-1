27c4b1e991deed7ba0e19b540ea497727b7bfc0a
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-13 14:50:20 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/yamamoto64/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.yamamoto64.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yamamoto64.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	User findByEmail(String email);
+}

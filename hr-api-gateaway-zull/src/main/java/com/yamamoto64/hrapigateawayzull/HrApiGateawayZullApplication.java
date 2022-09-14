72db1b923bbd12347bbfaf1f82e99adfd6a57917
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-14 13:24:29 -0300

    Security configs to config server

--- hr-api-gateaway-zull/src/main/java/com/yamamoto64/hrapigateawayzull/HrApiGateawayZullApplication.java
+++ hr-api-gateaway-zull/src/main/java/com/yamamoto64/hrapigateawayzull/HrApiGateawayZullApplication.java
@@ -2,7 +2,6 @@ package com.yamamoto64.hrapigateawayzull;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 

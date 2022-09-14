0d5099f2b0eb2acc5bf018dcd11b2d6dc8a72118
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-14 11:38:14 -0300

    Zuul Authorization

--- hr-api-gateaway-zull/src/main/java/com/yamamoto64/hrapigateawayzull/HrApiGateawayZullApplication.java
+++ hr-api-gateaway-zull/src/main/java/com/yamamoto64/hrapigateawayzull/HrApiGateawayZullApplication.java
@@ -2,6 +2,7 @@ package com.yamamoto64.hrapigateawayzull;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
